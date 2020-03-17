39dad1b6e361bd06f135c8481a08b9c014c6b313
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-03-17 16:07:44 -0600

    MVP

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,11 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  console.log("this is the comments", props.comments);
+ 
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {props.comments.map((item,index)=>{
+        console.log("this is new ITEM",item);
+        return <Comment key = {index} comment = {item} />
+      })}
+      <CommentInput comments = {props.comments}/>
     </div>
   );
 };
